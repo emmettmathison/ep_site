d74edd3d0c12fe13336f57aa33833d025cf594d7
Crash Override <emmettmathison@gmail.com>
2020-04-09 13:20:37 -0700

    initial commit- font end

---
+++ routes/HTML.js
@@ -0,0 +1,11 @@
+var path = require("path");
+
+module.exports = function(app) {
+    app.get("/listen", function(req, res) {
+        res.sendFile(path.join(__dirname, "../client/listen.html"));
+    });
+
+    app.get("*", function(req, res) {
+        res.sendFile(path.join(__dirname, "../client/landing.html"));
+    })
+}
