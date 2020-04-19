a05f3021ed46ba745b3365ad6c67c6e38cadaae1
Crash Override <emmettmathison@gmail.com>
2020-04-19 15:41:14 -0700

    some more stuff

--- client/assets/js/listen.js
+++ client/assets/js/listen.js
@@ -6,10 +6,16 @@ audioPlayer();
 
 var btnValue = dropdownBase.attr("value");
 
-var songNames = ["word", "guoying", "sound", "trona", "day"];
+var borderNames = ["word", "guoying", "sound", "trona", "day"];
+var textNames = [
+  "word",
+  "guoying",
+  "sound",
+  "trona",
+  "day",
+];
 
 nextSong();
-
 
 dropdownBase.on("click", function () {
   if (btnValue === "unclicked") {
@@ -33,66 +39,63 @@ function audioPlayer() {
   $("#word-deg0").click(function (e) {
     $("#audioPlayer")[0].src = $("#songs li a")[0];
     $("#audioPlayer")[0].play();
-    $(".circle").css({"border": "3px solid white"});
-    $(".titles").css({"color": "white"})
-    this.style.border = '3px solid rgb(250, 49, 234)'
-    $(this).children().css({"color" : "rgb(250, 49, 234)"});
+    $(".circle").css({ border: "3px solid white" });
+    $(".titles").css({ color: "white" });
+    this.style.border = "3px solid rgb(250, 49, 234)";
+    $(this).children().css({ color: "rgb(250, 49, 234)" });
     currentSong = $(this).index();
   });
   $("#guoying-deg72").click(function (e) {
     $("#audioPlayer")[0].src = $("#songs li a")[1];
     $("#audioPlayer")[0].play();
-    $(".circle").css({"border": "3px solid white"});
-    $(".titles").css({"color": "white"})
-    this.style.border = '3px solid rgb(250, 49, 234)'
-    $(this).children().css({"color" : "rgb(250, 49, 234)"});
+    $(".circle").css({ border: "3px solid white" });
+    $(".titles").css({ color: "white" });
+    this.style.border = "3px solid rgb(250, 49, 234)";
+    $(this).children().css({ color: "rgb(250, 49, 234)" });
     currentSong = $(this).index();
   });
   $("#sound-deg144").click(function (e) {
     $("#audioPlayer")[0].src = $("#songs li a")[2];
     $("#audioPlayer")[0].play();
-    $(".circle").css({"border": "3px solid white"});
-    $(".titles").css({"color": "white"})
-    this.style.border = '3px solid rgb(250, 49, 234)'
-    $(this).children().css({"color" : "rgb(250, 49, 234)"});
+    $(".circle").css({ border: "3px solid white" });
+    $(".titles").css({ color: "white" });
+    this.style.border = "3px solid rgb(250, 49, 234)";
+    $(this).children().css({ color: "rgb(250, 49, 234)" });
     currentSong = $(this).index();
   });
   $("#trona-deg216").click(function (e) {
     $("#audioPlayer")[0].src = $("#songs li a")[3];
     $("#audioPlayer")[0].play();
-    $(".circle").css({"border": "3px solid white"});
-    $(".titles").css({"color": "white"})
-    this.style.border = '3px solid rgb(250, 49, 234)'
-    $(this).children().css({"color" : "rgb(250, 49, 234)"});
+    $(".circle").css({ border: "3px solid white" });
+    $(".titles").css({ color: "white" });
+    this.style.border = "3px solid rgb(250, 49, 234)";
+    $(this).children().css({ color: "rgb(250, 49, 234)" });
     currentSong = $(this).index();
   });
   $("#withDay-deg288").click(function (e) {
     $("#audioPlayer")[0].src = $("#songs li a")[4];
     $("#audioPlayer")[0].play();
-    $(".circle").css({"border": "3px solid white"});
-    $(".titles").css({"color": "white"})
-    this.style.border = '3px solid rgb(250, 49, 234)'
-    $(this).children().css({"color" : "rgb(250, 49, 234)"});
+    $(".circle").css({ border: "3px solid white" });
+    $(".titles").css({ color: "white" });
+    this.style.border = "3px solid rgb(250, 49, 234)";
+    $(this).children().css({ color: "rgb(250, 49, 234)" });
     currentSong = $(this).index();
   });
 }
 
-// illuminate();
-
 function nextSong() {
-  $("#audioPlayer")[0].addEventListener("ended", function() {
+  $("#audioPlayer")[0].addEventListener("ended", function () {
     currentSong++;
     $("#audioPlayer")[0].src = $("#songs li a")[currentSong].href;
-    console.log($(".circle-container").children()[currentSong-1]);
     $("#audioPlayer")[0].play();
-  }
-  )}
+    illuminate();
+  });
+}
 
-  // function illuminate() {
-  //     if($("#audioPlayer")[0].src = $("#songs li a")[1].href) {
-  //       $(".guoying").css({"border" : "3px solid rgb(250, 49, 234)"})
-  //     }
-  // }
-
-
-
+function illuminate() {
+  $(".circle").css({ border: "3px solid white" });
+    $(".titles").css({ color: "white" });
+    $("." + borderNames[currentSong]).css({"border" : "3px solid rgb(250, 49, 234)"});
+    $("#" + textNames[currentSong]).css({"color" : "rgb(250, 49, 234)"});
+    
+}
