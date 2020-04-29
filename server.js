175712b35cbe7acb999327f9bca32b1e21f2f055
Crash Override <emmettmathison@gmail.com>
2020-04-29 16:54:28 -0700

    some changes for heroku

--- server.js
+++ server.js
@@ -3,14 +3,8 @@ var path = require("path");
 var app = express();
 var PORT = process.env.PORT;
 
-const PORT = process.env.PORT || 5000;
+require("./routes/HTML")(app);
 
 express()
   .use(express.static(path.join(__dirname, "client")))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
-
-require("./routes/HTML")(app);
-
-app.listen(PORT, function () {
-  console.log(`Now listening on port: ${PORT}`);
-});
