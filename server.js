ca6f6e11953b83aff6e584036bb26d077d122ae9
Crash Override <emmettmathison@gmail.com>
2020-05-01 18:05:16 -0700

    query test 2

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 
 var app = express();
-var PORT = process.env.PORT;
+var PORT = process.env.PORT || 8080;
 
 app.use(express.static("client"));
 app.use(express.urlencoded({ extended: true }));
